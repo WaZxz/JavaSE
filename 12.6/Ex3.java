986679acabb5f1e8dc6fec41a0f586ceab4424e0
冯炜博 <190610977@qq.com>
2018-12-09 23:13:32 +0800

    代码上传

---
+++ 12.6/Ex3.java
@@ -0,0 +1,22 @@
+class Person{
+    private String name;
+    private int age;
+    public Person(String name,int age){
+    this.name = name ;
+    this.age = age ;
+    this.print();//调用普通方法
+    }
+    public String getPersonInfo(){
+    return "姓名：" + name + ",年龄："+age;
+    }
+    public void print(){
+    System.out.println("*****************");
+    }
+    }
+    public class Ex3{
+    public static void main(String[] args) {
+    Person per = new Person("张三",20);
+    System.out.println(per.getPersonInfo());
+    }
+    }
+    
