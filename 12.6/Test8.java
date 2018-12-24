09ae10a20e776280dc381e9ee4e6324fcfa7c5f4
冯炜博 <190610977@qq.com>
2018-12-24 21:40:31 +0800

    加油

---
+++ 12.6/Test8.java
@@ -0,0 +1,25 @@
+
+interface IColor {
+public String getColor() ;
+}
+enum Color implements IColor{
+RED("红色"),GREEN("绿色"),BLUE("蓝色") ; // 如果定义有很多内容，枚举对象必须写在第一行
+private String title ;
+private Color(String title) { // 构造方法私有化
+this.title = title ;
+}
+@Override
+public String toString() {
+return this.title ;
+}
+@Override
+public String getColor() {
+return this.title ;
+}
+}
+public class Test8 {
+public static void main(String[] args) {
+IColor iColor = Color.BLUE ;
+System.out.println(iColor.getColor());
+}
+}
